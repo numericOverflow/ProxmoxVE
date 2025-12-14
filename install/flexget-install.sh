54de7f5968b2c93f6621266404685f9ae5f3f369
numericOverflow <numericOverflow@users.noreply.github.com>
2025-12-14 08:55:41 -0600

    rewrite service code

--- install/flexget-install.sh
+++ install/flexget-install.sh
@@ -25,18 +25,22 @@ msg_info "Adding flexget bin to PATH"
   source ~/.bashrc
 msg_ok "PATH updated"
 
+msg_info "Creating cofing directory"
+mkdir /etc/flexget
+msg_ok "Created /etc/flexget"
+
+msg_info "Creating symlinks to config for easy access"
+ln -s /root/.flexget /etc/flexget
+ln -s /root/flexget /etc/flexget
+msg_ok "Symlink '/root/flexget' added"
+
 msg_info "Installing FlexGet (uv-based version)"
 $STD uv tool install --python 3.13 flexget[locked,all]
 msg_ok "Installed FlexGet"
 
-msg_info "Creating symlink to config for easy access"
-mkdir /root/.flexget
-ln -s /root/.flexget /root/flexget
-msg_ok "Symlink '/root/flexget' added"
-
 msg_info "Setup FlexGet log rotation"
 cat <<EOF > /etc/logrotate.d/flexget-log
-/root/.flexget/flexget.log {
+/etc/flexget/flexget.log {
     daily
     rotate 30
     compress
@@ -50,7 +54,7 @@ msg_ok "Log rotation added"
 
 msg_info "Creating basic FlexGet config.yml"
 TEMP_CONFIG_FILE=$(mktemp) 
-FLEXGET_CONFIG_FILE="/root/.flexget/config.yml"
+FLEXGET_CONFIG_FILE="/etc/flexget/config.yml"
 mkdir -p "$(dirname "${FLEXGET_CONFIG_FILE}")"
 
 if [ -f "${FLEXGET_CONFIG_FILE}" ]; then
@@ -79,35 +83,39 @@ EOF
     fi
 fi
 
-msg_ok "Created /root/.flexget/config.yml"
-msg_ok "You should edit /root/.flexget/config.yml to suite your needs"
+msg_ok "Created /etc/flexget/config.yml"
+msg_ok "You should edit /etc/flexget/config.yml to suite your needs"
 
-echo -e "${INFO}${YW} Starting FlexGet daemon${CL}"
-flexget daemon start -d --autoreload-config
-echo -e ""
-msg_ok "Started FlexGet"
+#echo -e "${INFO}${YW} Starting FlexGet daemon${CL}"
+#flexget daemon start -d --autoreload-config
+#echo -e ""
+#msg_ok "Started FlexGet"
 
 msg_info "Setup flexget to run at startup"
 cat <<EOF >/etc/systemd/system/flexget.service
 [Unit]
-Description=Flexget Automation Daemon
+Description=Flexget Daemon
 After=network.target
 
 [Service]
 Type=simple
 User=root
 Group=root
-ExecStart=/usr/local/bin/flexget daemon start --autoreload-config
-Restart=always
-RestartSec=5s
-#StandardOutput=journal
-#StandardError=journal
+UMask=000
+WorkingDirectory=/etc/flexget
+ExecStart=/root/.local/bin/flexget daemon start --autoreload-config
+ExecStop=/root/.local/bin/flexget daemon stop
+ExecReload=/root/.local/bin/flexget daemon reload --autoreload-config
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+echo -e "${INFO}${YW} Starting FlexGet daemon${CL}"
 systemctl enable -q --now flexget
+echo -e ""
+msg_ok "Started FlexGet"
+
 
 msg_info "Cleaning up"
 #rm -f "${temp_file}"
